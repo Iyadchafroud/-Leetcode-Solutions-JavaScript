a3a1f9d32a8e153accd403ab9603355885a91e70
mohamed iyed chafroud <60520088+Iyadchafroud@users.noreply.github.com>
2023-02-27 21:08:14 +0100

    Time: 76 ms (56.43%), Space: 44.1 MB (59.54%) - LeetHub

---
+++ 0083-remove-duplicates-from-sorted-list/0083-remove-duplicates-from-sorted-list.js
@@ -0,0 +1,27 @@
+/**
+ * Definition for singly-linked list.
+ * function ListNode(val, next) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.next = (next===undefined ? null : next)
+ * }
+ */
+/**
+ * @param {ListNode} head
+ * @return {ListNode}
+ */
+function deleteDuplicates(head) {
+  if (head == null || head.next == null) {
+    return head;
+  }
+  
+  let curr = head;
+  while (curr.next != null) {
+    if (curr.val == curr.next.val) {
+      curr.next = curr.next.next;
+    } else {
+      curr = curr.next;
+    }
+  }
+  
+  return head;
+}
