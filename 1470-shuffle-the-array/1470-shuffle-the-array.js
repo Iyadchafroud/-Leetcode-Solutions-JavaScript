661477ded3775e53d95e89395975b9957f28ae3d
mohamed iyed chafroud <60520088+Iyadchafroud@users.noreply.github.com>
2023-03-05 15:51:21 +0100

    Time: 71 ms (74.19%), Space: 44.6 MB (45.28%) - LeetHub

--- 1470-shuffle-the-array/1470-shuffle-the-array.js
+++ 1470-shuffle-the-array/1470-shuffle-the-array.js
@@ -7,16 +7,12 @@ var shuffle = function (nums, n) {
   const firstshink = nums.slice(0, n);
   const secondshink = nums.slice(n, nums.length);
   const reservedArray = [];
-  let ind = 0;
-  let j = 0;
-  for (let index = 0; index < 2 * n; index++) {
-    if (index % 2 === 0) {
-      reservedArray[index] = firstshink[ind];
-      ind = ind + 1;
-    } else {
-      reservedArray[index] = secondshink[j];
-      j = j + 1;
-    }
+ 
+  for (let index = 0; index <  n; index++) {
+   reservedArray.push(firstshink[index])
+         reservedArray.push(secondshink[index])
+
+      
   }
 
   return reservedArray;
