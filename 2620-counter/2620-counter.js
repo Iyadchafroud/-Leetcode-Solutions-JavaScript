0af202acc4c7807fe31faa6fd974f6cc150892b1
mohamed iyed chafroud <60520088+Iyadchafroud@users.noreply.github.com>
2023-05-13 19:20:47 +0200

    Time: 61 ms (34.34%), Space: 41.7 MB (77.83%) - LeetHub

---
+++ 2620-counter/2620-counter.js
@@ -0,0 +1,20 @@
+/**
+ * @param {number} n
+ * @return {Function} counter
+ */
+var createCounter = function(n) {
+  let prev = n-1
+    return function() {
+        prev+=1
+        return prev
+      
+    };
+
+};
+
+/** 
+ * const counter = createCounter(10)
+ * counter() // 10
+ * counter() // 11
+ * counter() // 12
+ */
