5e8c6265ae05692f661121a57e9f92a350b70e2f
mohamed iyed chafroud <60520088+Iyadchafroud@users.noreply.github.com>
2023-05-29 22:46:59 +0200

    Time: 156 ms (61.80%), Space: 89.6 MB (46.85%) - LeetHub

---
+++ 2625-flatten-deeply-nested-array/2625-flatten-deeply-nested-array.js
@@ -0,0 +1,29 @@
+
+/**
+ * @param {any[]} arr
+ * @param {number} depth
+ * @return {any[]}
+ */
+var flat = function (arr, n) {
+    let nestedArrayElement = true;
+    let queue;
+    let depth = 0;
+
+    while(nestedArrayElement && depth < n) {
+        nestedArrayElement = false;
+        queue = [];
+
+        for(let i = 0; i < arr.length; i++) {
+            if(Array.isArray(arr[i])) {
+                queue.push(...arr[i]);
+                nestedArrayElement = true;
+            } else {
+                queue.push(arr[i]);
+            }
+        }
+        arr = [...queue];
+        depth++;
+    }
+    
+    return arr;
+};
