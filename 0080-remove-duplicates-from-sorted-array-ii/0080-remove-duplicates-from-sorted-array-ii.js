670b12d2c0a50f0a24764e5b509096f7ed006960
mohamed iyed chafroud <60520088+Iyadchafroud@users.noreply.github.com>
2024-02-17 20:51:01 +0100

    Time: 61 ms (61.47%), Space: 51.3 MB (39.13%) - LeetHub

---
+++ 0080-remove-duplicates-from-sorted-array-ii/0080-remove-duplicates-from-sorted-array-ii.js
@@ -0,0 +1,17 @@
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+var removeDuplicates = function(nums) {
+ let j = 0;
+  let i = 0;
+
+  for (; i < nums.length; i += 1){
+    if (nums[i] !== nums[i + 2]) {
+      nums[j] = nums[i];
+      j += 1;
+    }
+  }
+
+  return j
+}
