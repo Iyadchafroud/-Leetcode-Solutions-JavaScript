87dbc6a2f9190af1327ada16de207650a99034df
mohamed iyed chafroud <60520088+Iyadchafroud@users.noreply.github.com>
2024-02-26 19:27:29 +0100

    Time: 60 ms (39.93%), Space: 52.4 MB (19.30%) - LeetHub

---
+++ 0704-binary-search/0704-binary-search.js
@@ -0,0 +1,17 @@
+/**
+ * @param {number[]} nums
+ * @param {number} target
+ * @return {number}
+ */
+var search = function(nums, target) {
+    let lo = 0, hi = nums.length-1;
+    while (lo < hi) {
+        let mid = lo + Math.floor((hi-lo+1)/2);
+        if (target < nums[mid]) {
+            hi = mid - 1
+        } else {
+            lo = mid; 
+        }
+    }
+    return nums[lo]==target?lo:-1;
+};
