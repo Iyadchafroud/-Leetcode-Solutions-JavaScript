a440406c2977e0aac88e270117efaff80ff93bc3
mohamed iyed chafroud <60520088+Iyadchafroud@users.noreply.github.com>
2024-03-13 17:31:44 +0100

    Time: 62 ms (40.00%), Space: 48.8 MB (65.13%) - LeetHub

---
+++ 2485-find-the-pivot-integer/2485-find-the-pivot-integer.js
@@ -0,0 +1,17 @@
+/**
+ * @param {number} n
+ * @return {number}
+ */
+var pivotInteger = function(n) {
+     // Iterate through all possible values of x
+    for (let i = 1; i <= n; i++) {
+        let leftSum = (i * (i + 1)) / 2; 
+        let rightSum = (n * (n + 1)) / 2 - (i * (i - 1)) / 2;
+        
+   
+        if (leftSum === rightSum)
+            return i;
+    }
+    
+    return -1
+};
